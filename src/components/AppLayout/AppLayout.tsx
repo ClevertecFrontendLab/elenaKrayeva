c25d3aadd1b41691df3cfa2291bb516291a048b8
Elena.Krayeva <alenakrayeva@gmail.com>
2025-04-15 03:33:05 +0300

    fix(ui): change layout, remove scroll

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { Outlet } from 'react-router-dom';
 
@@ -8,7 +8,6 @@ import { Notifications } from '~/components/Notifications/Notifications';
 import { SidebarMenu } from '~/components/SidebarMenu/SidebarMenu';
 import {
     FOOTER_HEIGHT,
-    FOOTER_MOBILE_HEIGHT,
     HEADER_HEIGHT,
     LEFT_SIDEBAR_WIDTH,
     RIGHT_SIDEBAR_WIDTH,
@@ -27,7 +26,7 @@ export const AppLayout = () => {
                 as='header'
                 bg='lime.50'
                 h={HEADER_HEIGHT}
-                position='sticky'
+                position='fixed'
                 top='0'
                 left='0'
                 right='0'
@@ -43,73 +42,76 @@ export const AppLayout = () => {
                 </Container>
             </Box>
 
-            <Container size='xl' height={`calc(100vh - ${HEADER_HEIGHT})`} pl='0px' pr='0'>
-                <Flex width='100%' height={`calc(100vh - ${HEADER_HEIGHT})`}>
-                    <Box
-                        height={`calc(100vh - ${HEADER_HEIGHT})`}
-                        width={LEFT_SIDEBAR_WIDTH}
-                        overflow='hidden'
-                        position='relative'
-                        borderRight='1px solid'
-                        borderColor='blackAlpha.400'
-                        display={{ base: 'none', lg: 'block' }}
-                    >
-                        <Box
-                            as='aside'
-                            ref={asideRef}
-                            height='calc(100% - 100px)'
-                            overflow='auto'
-                            mr={1}
-                            position='relative'
-                            bg='white'
-                            zIndex={asideHasScroll ? 110 : undefined}
-                            borderBottom={asideHasScroll ? '1px solid' : 'none'}
-                            borderColor={asideHasScroll ? 'blackAlpha.400' : 'transparent'}
-                            borderBottomLeftRadius={asideHasScroll ? '12px' : '0'}
-                            borderBottomRightRadius={asideHasScroll ? '12px' : '0'}
-                            boxShadow={asideHasScroll ? 'md' : 'none'}
-                        >
-                            <SidebarMenu />
-                        </Box>
-                        <Box
-                            as='footer'
-                            bg='white'
-                            height={FOOTER_HEIGHT}
-                            position='absolute'
-                            bottom='0'
-                            left='0'
-                            right='0'
-                            zIndex='100'
-                        >
-                            <Footer />
-                        </Box>
-                    </Box>
-                    <Box
-                        as='main'
-                        height={{
-                            base: `calc(100vh - ${HEADER_HEIGHT} - ${FOOTER_MOBILE_HEIGHT})`,
-                            md: `calc(100vh - ${HEADER_HEIGHT} - ${FOOTER_MOBILE_HEIGHT})`,
-                            lg: `calc(100vh - ${HEADER_HEIGHT})`,
-                        }}
-                        flex='1'
-                        overflowY='auto'
-                        overflowX='hidden'
-                        px={{ base: '16px', md: '20px', lg: '24px' }}
-                        mb={{ base: '100px', lg: 0 }}
-                    >
-                        <Outlet />
-                    </Box>
-                    <Box
-                        as='aside'
-                        height={`calc(100vh - ${HEADER_HEIGHT})`}
-                        width={RIGHT_SIDEBAR_WIDTH}
-                        pl='36px'
-                        display={{ base: 'none', lg: 'block' }}
-                    >
-                        <Notifications />
-                    </Box>
-                </Flex>
-            </Container>
+            <Box
+                as='aside'
+                position='fixed'
+                top={HEADER_HEIGHT}
+                left='0'
+                height={`calc(100vh - ${HEADER_HEIGHT})`}
+                width={LEFT_SIDEBAR_WIDTH}
+                borderRight='1px solid'
+                borderColor='blackAlpha.400'
+                bg='white'
+                display={{ base: 'none', lg: 'block' }}
+                zIndex='101'
+                flexShrink={0}
+            >
+                <Box
+                    ref={asideRef}
+                    height='calc(100% - 100px)'
+                    overflow='auto'
+                    p={2}
+                    position='relative'
+                    bg='white'
+                    zIndex={asideHasScroll ? 110 : undefined}
+                    borderBottom={asideHasScroll ? '1px solid' : 'none'}
+                    borderColor={asideHasScroll ? 'blackAlpha.400' : 'transparent'}
+                    borderBottomLeftRadius={asideHasScroll ? '12px' : '0'}
+                    borderBottomRightRadius={asideHasScroll ? '12px' : '0'}
+                    boxShadow={asideHasScroll ? 'md' : 'none'}
+                >
+                    <SidebarMenu />
+                </Box>
+                <Box
+                    as='footer'
+                    bg='white'
+                    height={FOOTER_HEIGHT}
+                    position='absolute'
+                    bottom='0'
+                    left='0'
+                    right='0'
+                    zIndex='100'
+                >
+                    <Footer />
+                </Box>
+            </Box>
+
+            <Box
+                as='aside'
+                position='fixed'
+                top={HEADER_HEIGHT}
+                right='0'
+                height={`calc(100vh - ${HEADER_HEIGHT})`}
+                width={RIGHT_SIDEBAR_WIDTH}
+                pl='36px'
+                bg='white'
+                display={{ base: 'none', lg: 'block' }}
+                zIndex='101'
+            >
+                <Notifications />
+            </Box>
+
+            <Box
+                as='main'
+                ml={{ base: '0', lg: LEFT_SIDEBAR_WIDTH }}
+                mr={{ base: '0', lg: RIGHT_SIDEBAR_WIDTH }}
+                pt={HEADER_HEIGHT}
+                px={{ base: '16px', md: '20px', lg: '24px' }}
+                mb={{ base: '100px', lg: 0 }}
+            >
+                <Outlet />
+            </Box>
+
             <Box display={{ base: 'flex', md: 'flex', lg: 'none' }}>
                 <MobileFooter />
             </Box>
